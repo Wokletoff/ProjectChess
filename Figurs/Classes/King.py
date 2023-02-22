24b209f6b13e52cc792ad8f75918cf1735642f99
Wokletoff <telkov2000@yandex.ru>
2023-02-22 11:22:44 +0300

    redact files path

--- Figurs/Classes/King.py
+++ Figurs/Classes/King.py
@@ -7,7 +7,7 @@ class King(Piece):
     def __init__(self, pos, color, board):
         super().__init__(pos, color, board)
 
-        img_path = 'Figurs/Imgs/' + color[0] + '_king.png'
+        img_path = 'data/imgs/' + color[0] + '_king.png'
         self.img = pygame.image.load(img_path)
         self.img = pygame.transform.scale(self.img, (board.tile_width - 20, board.tile_height - 20))
 
