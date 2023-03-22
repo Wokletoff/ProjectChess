6ab29c0b3df32426a7404f20afb70db134a42d5c
Wokletoff <telkov2000@yandex.ru>
2023-03-22 10:47:49 +0300

    Push

---
+++ Pucsh.py
@@ -0,0 +1,33 @@
+import subprocess
+
+class Pusher:
+ def Push(self):
+    files = ["Chat.py", "Server.py"]  # файлы, которые нужно запустить
+    for file in files:
+        subprocess.Popen(args=["start", "python", file], shell=True, stdout=subprocess.PIPE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
